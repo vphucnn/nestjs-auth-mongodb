349871d40cbbc03da97e05bad258a4f431dc6204
vphucnn <140361269+vphucnn@users.noreply.github.com>
2024-07-22 08:33:23 +0700

    config prettier end of line auto

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,4 +23,10 @@ module.exports = {
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-unused-vars': ['warn', { ignoreRestSiblings: true }],
   },
+  "prettier/prettier": [
+    "error",
+    {
+      "endOfLine": "auto"
+    }
+  ]
 };
